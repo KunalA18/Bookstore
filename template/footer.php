cd3c5cf49c17ab337300e1b10437aeec5e872207
Kunal Agarwal <kunalagarwal1072002@gmail.com>
2022-11-23 08:19:46 +0530

    added files and directories

---
+++ template/footer.php
@@ -0,0 +1,14 @@
+      	<hr>
+        <footer>
+        	<div class="text-muted pull-left">
+            	<a target="_blank"> Created by Jash and Kunal </a>
+        	</div>
+        	<div class="text-muted pull-right">
+          		<a href="admin.php">Admin Login</a> 2017
+        	</div>
+      	</footer>
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="./bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
